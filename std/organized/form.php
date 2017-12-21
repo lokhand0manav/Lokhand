1f2aa9a718c347676d76ab89ad87bd41644e55c2
Hardik0707 <thakkar.h@somaiya.edu>
2017-12-21 17:38:10 +0530

    Updated Organized

--- std/organized/form.php
+++ std/organized/form.php
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 		//the form was submitted
     
 		$ivdate_array = $_POST['ivdate'];
-		$fname_array = $_POST['fname'];
+		//$fname_array = $_POST['fname'];
 		$ind_array = $_POST['ind'];
 		$city_array = $_POST['city'];
 		$purpose_array = $_POST['purpose'];
@@ -54,7 +54,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 		for($i=0; $i<count($city_array);$i++)
 		{
 			$ivdate = mysqli_real_escape_string($conn,$ivdate_array[$i]);
-			$fname = mysqli_real_escape_string($conn,$fname_array[$i]);
+			$fname = $username;
+			//$fname = mysqli_real_escape_string($conn,$fname_array[$i]);
 			$ind = mysqli_real_escape_string($conn,$ind_array[$i]);
 			$city = mysqli_real_escape_string($conn,$city_array[$i]);
 			$purpose = mysqli_real_escape_string($conn,$purpose_array[$i]);	
@@ -68,19 +69,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 				$dateerr="Please enter a date";
 				$flag++;
 			}
-			if(empty($_POST['fname[]']))
-			{
-				$fnameerr="Enter a name";
-				$flag++;
-			}else 
-				{
-					$name = test_input($_POST['fname[]']);
-					if (!preg_match("/^[a-zA-Z]*$/",$name))
-					{
-						$fnameerr = "Only letters and whitespace allowed";
-						$flag++;
-					}
-				}
 			if(empty($_POST['sname[]']))
 			{
 				$snameerr="Enter a name";
@@ -130,124 +118,120 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 				$flag++;
 			}
 
-			$sql="INSERT INTO organized (f_name,ind,city,purpose,date,t_audience,staff,permission,report,certificate,attendance,t_from,t_to) VALUES ('$fname','$ind','$city','$purpose','$ivdate','$t_audience','$staff','','','','','$from','$to')";
+			$sql="INSERT INTO organized (f_name,ind,city,purpose,date,t_audience,s_name) VALUES ('$fname','$ind','$city','$purpose','$ivdate','$t_audience','$staff')";
+
 			if(!mysqli_query($conn,$sql))
 			{
 				echo"Not Inserted";
 			}
 			else
 			{
-				//echo"Record Inserted Successfully !";
+				echo"Record Inserted Successfully !";
+				header("refresh:1; url=template.php?x=../organized/view.php");
+				exit(0);
 				
 			}
 		}
 				mysqli_close($conn);
-				header("refresh:1; url=template.php?x=../organized/view.php");
+				
 	}
 }
 
 ?>
+	
 
 
+	
+    
+    <section class="content">
+          <div class="row">
+            <!-- left column -->
+            <div class="col-md-6">
+              <!-- general form elements -->
+              <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">IV Activity Organized Form</h3>
+				 
+                </div><!-- /.box-header -->
+                <!-- form start -->
+	
+				<div class="form-group col-md-10">
 
-	<section class="content-header">
-		<div class="container " style="border: 1px solid black;padding: 10px 60px;width:50%">
-			<h2>Organized</h2>
-			<label for="faculty-name">Faculty Name</label>
+                         <label for="faculty-name">Faculty Name</label>
                          <input required type="text" class="form-control input-lg" id="faculty-name" name="facultyName" value="<?php echo $username; ?>" readonly>
-			<?php
+                     </div><br/> <br/> <br/> <br/> 
+				
+				<?php
 			
 					for($k=0; $k<$_SESSION['count'] ; $k++)
 					{
 
 				?>
-				________________________________________________________
-			<form class="form-horizontal" method="post" action="">
-
-				<div class="form-group">
-					<label>Date of visit:</label>
+            <p>***************************************************************************************</p>
+			<form role="form" method="POST" class="row" action= "template.php?x=../organized/form.php" style= "margin:10px;" >
+					
+				
+                <div class="form-group col-md-6">
+               	<label>Date of Visit:</label><span class="required">*</span>
 					<input type="date" name="ivdate[]" class="form-control">
 					<span class="error"><?php echo $dateerr; ?></span>
-				</div>
+                </div>
 
-				<div class="form-group">
-					<label >Faculty Name:</label>      
-					<input type="hidden" class="form-control" name="fname[]" value="<?php echo $username; ?>"><?php echo $username; ?>
-					<span class="error"><?php echo $fnameerr; ?></span>
-				</div>
-
-				<!--<div class="form-group">
-					<label >Staff Name:</label>      
-					<input type="text" class="form-control" name="sname[]">
-					<span class="error"><?php echo $snameerr; ?></span>
-				</div>-->
-
-				<div class="form-group">
-					<label >Industry Details</label>         
-					<input type="textarea" rows="5" cols="5" class="form-control" name="ind[]">
+                     <div class="form-group col-md-6"><span class="required">*</span>
+                         <label >Industry Name</label>         
+						<input type="text" class="form-control" name="ind[]">
 					<span class="error"><?php echo $inderr; ?></span>
-				</div>
+                     </div>
+                     <div class="form-group col-md-12">
+                         <label >Purpose</label><span class="required">*</span>        
+          				<textarea rows="5" cols="5" class="form-control" name="purpose[]">
+          				</textarea>
+          				<span class="error"><?php echo $perr; ?></span>
+                     </div>
 
-				<div class="form-group">
-					<label>City</label>
+                     <div class="form-group col-md-8"> 
+                     <label>City</label><span class="required">*</span>
 					<input type="text" class="form-control" name="city[]">
 					<span class="error"><?php echo $cityerr; ?></span>
-				</div>
-
-				<div class="form-group">
-					<label>Purpose</label>
-					<input type="text" class="form-control" name="purpose[]">
-					<span class="error"><?php echo $perr; ?></span>
-				</div>
-
-				<div class="form-group">
-					<label>Target Audience</label>
+                         
+                     </div>
+                     
+                     <div class="form-group col-md-8"> 
+                     <label>Target Audience</label><span class="required">*</span>
 					<input type="text" class="form-control" name="t_audience[]">
 					<span class="error"><?php echo $taerror; ?></span>
-				</div>
+					</div>
 
-				<div class="form-group">
-					<label>Staff</label>
+
+					<div class="form-group col-md-8">
+					<label>Staff</label><span class="required"> *</span>
 					<input type="text" class="form-control" name="staff[]">
 					<span class="error"><?php echo $serror; ?></span>
-				</div>
+					</div>
 
+					<div class="form-group col-md-12">
 
-				<div class="form-group">
-					<label>Duration</label><br>
-					From: <input type="date" name="from[]" placeholder="from">
-					To :<input type="date" name="to[]" placeholder="to"><br>
-				</div>  
+					<label>Duration</label><span class="required"> *</span>
+					<br>
+					<b>From:</b> &emsp;<input type="date" name="from[]" placeholder="from">
+					
+					&emsp;
+					<b>To:</b>&emsp;<input type="date" name="to[]" placeholder="to"><br>
+					</div>
 
-				<!--<div class="form-group">
-					<label>Attach Attendance Record</label><br>
-					<input type="file" class="btn btn-file" value="Browse.." name="attendance" id="attendance">
-				</div>
-
-				<div class="form-group">
-					<label>Attach Permission Letter</label><br>
-					<input type="file" class="btn btn-file" value="Browse..." name="permission" id="permission">
-				</div>  
-				<div class="form-group">
-					<label>Attach Report</label><br>
-					<input type="file" class="btn btn-file" value="Browse.." name="report" id="report">
-				</div>
-
-				<div class="form-group">
-					<label>Attach Certificate</label><br>
-					<input type="file" class="btn btn-file" value="Browse..." name="certificate" id="certificate">
-				</div>  -->
-				<?php
+                   <?php
 					}
-				?>
+					?>
+					<br/>
+                    <div class="form-group col-md-12">
+                         <a href="template.php?x=../select_menu/addcount.php" type="button" class="btn btn-warning btn-lg">Cancel</a>
 
-				<div class="form-group">        
-					<button type="submit" name="add" class="btn btn-primary">Submit</button>
-				</div>
-			</form>
-
-		</div>
-	</section>
-
+                         <input name="add" type="submit" class="btn btn-success pull-right btn-lg">
+                    </div>
+                </form>
+                </div>
+              </div>
+           </div>      
+        </section>
 </body>
  </html>
