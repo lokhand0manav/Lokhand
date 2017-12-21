a1d5d2cd39324f14e6dddf9324dc2cbc8f182cb4
Hardik0707 <thakkar.h@somaiya.edu>
2017-12-21 14:02:03 +0530

    Update head.php

--- std/Includes/head.php
+++ std/Includes/head.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<style type="text/css">
+	.required{
+		color:red;
+		}
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Organized View</title>
