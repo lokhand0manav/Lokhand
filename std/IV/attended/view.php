ac8b9f070c452bbc312bfa7fd70428ff2239dfdd
lokhand0manav <nsankpal97@gmail.com>
2017-12-23 13:46:39 +0530

    analysis added

--- std/IV/attended/view.php
+++ std/IV/attended/view.php
@@ -5,138 +5,138 @@ $(document).ready(function(){
 </script>
 
 <?php 
-if(session_status() == PHP_SESSION_NONE){
+ob_start();
+ if(session_status() == PHP_SESSION_NONE)
+   {
     //session has not started
     session_start();
-}
-  if($_SESSION['username']=="")
-  {
-  header("refresh:1,url=../../login.php");
-  die("Login Required");
-  }
-  else
-  {
-    $username = $_SESSION['username'];
-  }
-?>
+   }
+ if(!isset($_SESSION['username']))
+   {
+    header("refresh:2,url=../login.php");
+   }
+   
+include_once("../includes/connection.php");
+ ?>
 
-<?php
-        $flag="";
-        $sql="";
-        if(isset($_POST['view_attended']))
-        {
-        $flag= 1;
-        //echo "<center>".$_POST['from_date']."</center>";
-        $conn=mysqli_connect('localhost','root','','preyash');
-        $sql="SELECT * FROM attended where date >='$_POST[from_date]' AND date <='$_POST[to_date]' AND f_name ='$username' ";
-        
-        $records=mysqli_query($conn,$sql);
-        }
 
-        else if(isset($_POST['view_attended_all']))
-        {
-        $flag= 1;
-        echo "<center>".$_POST['from_date']."</center>";
-        $conn=mysqli_connect('localhost','root','','preyash');
-        $sql="SELECT * FROM attended where f_name='$username'"; 
-        $records=mysqli_query($conn,$sql);
-        }
-?>
 
-  
-      <!-- ============== View by particular Date =============== -->
-
-        <form role="form" action = "" method="post">
-          <div class="box-body">
-
-           <div class="form-group">
-            <label for="InputDateFrom">Date from :</label>
-            <input type="date" name="from_date">
-
-            <label for="InputDateTo">Date To :</label>
-            <input type="date" name="to_date"></p>
-        </div>
-    </div>
-
-    <div class="box-footer">
-        <input type="submit" class="btn btn-warning btn-lg" name="view_attended" value = "View"></input>
-        <input type="submit" class="btn btn-warning btn-lg" name="view_attended_all" value = "View All"></input>
-    </div>
-</form>
-
-        <!-- Main content -->
-        <section class="content">
-          <div class="row">
-            <div class="col-xs-12">
-              
-
-              <div class="box">
-                <div class="box-header">
-                  <h3 class="box-title">View-Attended</h3>
+              <!-- general form elements -->
+              <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Analysis</h3>
                 </div><!-- /.box-header -->
-                <div class="box-body">
-                  <table id="example1" class="table table-bordered table-striped">
-                    <thead>
-                      <tr>
-                        <th>Faculty name</th>
-                        <th>I.V Details</th>
-                        <th>Purpose</th>
-                        <th>Date</th>
-                        <th>Certificate</th>
-                        <th>permission</th>
-                     </tr>
-                    </thead>
-                    <tbody>
-                      <?php
-                      if($flag==1)
-                      {
-                        while($iv=mysqli_fetch_assoc($records))
-                        {
-                         echo"<tr>";
-                         echo"<td>".$iv['f_name']."</td>";
-                         echo"<td>".$iv['ind'].",".$iv['city']."</td>";
-                         echo"<td>".$iv['purpose']."</td>";
-                         echo"<td>".$iv['date']."</td>"; //$iv['certificate']
-                         //echo"<td>".$iv['certificate']."</td>";
-                         echo"<td><a href=".$iv['certificate']." download>download</a></td>";
-                          echo"<td>".$iv['permission']."</td>";
-  //echo"<td><img src=".$iv['permission']." alt='permission' height='42' width='42'>";
-                         echo"</tr>";
+                <!-- form start -->
+                <form role="form" action = "" method="post">
+                  <div class="box-body">
+                    
+					 <div class="form-group">
 
-                        }
-                       }
-                        
-                      ?>
-                    </tbody>
+						<input type="radio" name="choose" value="attended" <?php if($_SESSION['tbname'] == "attended"){ echo 'checked="checked"';}?>><label>Attended</label>&nbsp&nbsp&nbsp
+						<input type="radio" name="choose" value="organized" <?php if($_SESSION['tbname'] == "organized"){ echo 'checked="checked"';}?>><label>Organized</label>&nbsp&nbsp&nbsp
+						<input type="submit" class="btn btn-warning btn-lg" name="ok" value = "OK"></input><br><br>
+						
+			
+                        <label for="InputDateFrom">Date from :</label>
+						<input type="date" name="min_date">
+<p>
+ 						<label for="InputDateTo">Date To :</label>
+						<input type="date" name="max_date"></p>
+						<label>Faculty :</label>
+						<select name="fac">
+						<option value="Not Choosen">&nbsp&nbsp&nbspSelect&nbsp&nbsp&nbsp</option>
+						<?php	
+						
+						$_SESSION['tbname'] = $_POST['choose'];						
+						$tbname = $_SESSION['tbname'];
+						$sql="SELECT * FROM `{$tbname}`";
+						$records=mysqli_query($conn,$sql);
+						while($employee=mysqli_fetch_assoc($records))
+						{
+							$name=$employee['f_name'];
+							echo "<option value=".$name.">".$name."</option>";
+						}				
+						?>
+						</select>
+                    </div>
                    
-                  </table>
-                  <div>
-                    <?php 
-                        $_SESSION['table_name'] = 'attended';
-                        $_SESSION['table_query'] = $sql;
-                    ?>
-                    <a href="../../includes/export_to_excel.php" type="button" class="btn btn-success btn-sm"><span class="glyphicon ">Export</span></a>
-                  </div>
+                   
+                    
+                  </div><!-- /.box-body -->
 
-                </div><!-- /.box-body -->
-              </div><!-- /.box -->
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-        </section><!-- /.content -->
-      </div><!-- /.content-wrapper -->
-    <script>
-      $(function () {
-        $("#example1").DataTable();
-        $('#example2').DataTable({
-          "paging": true,
-          "lengthChange": false,
-          "searching": false,
-          "ordering": true,
-          "info": true,
-          "autoWidth": false
-        });
-      });
-    </script>
-    
-  </body>
-  </html>
+                  <div class="box-footer">
+                    <input type="submit" class="btn btn-warning btn-lg" name="count_total" value = "Analyize"></input>
+                    <a href="template.php?x=../IV/attended/addcount.php" type="button" class="btn btn-warning btn-lg">Back to Home </a>
+
+                  </div>
+				   <?php
+					$_SESSION['count1'] = 0;
+					//$_SESSION['tbname'] = $_POST['choose'];						
+					$tbname = $_SESSION['tbname'];
+					if(isset($_POST['min_date'])){
+						$_SESSION['from_date'] = $_POST['min_date'];
+						$_SESSION['to_date'] = $_POST['max_date'];
+						$from_date =  $_SESSION['from_date'] ;
+						$to_date = $_SESSION['to_date'] ;						
+					}
+					
+				   if(isset($_POST['count_total']))
+						{
+							if((strtotime($_POST['min_date']))<(strtotime($_POST['max_date'])))
+							{
+								$sql="SELECT f_name,ind,city,purpose,date FROM `$tbname` WHERE date >= '$from_date' and date <= '$to_date' ORDER BY DATE ASC";
+							}
+							else
+							{
+								$sql="SELECT f_name,ind,city,purpose,date FROM `$tbname` WHERE f_name='$_POST[fac]'";
+							}
+							$_SESSION['fac']=$_POST['fac'];
+							if($res1 = mysqli_query($conn,$sql))
+							{
+									if(mysqli_num_rows($res1) > 0)
+									{
+										while($row = $res1->fetch_assoc()) 
+										{
+											$fname[] = $row['f_name'];
+											$ind [] = $row['ind'];
+											$city[] = $row['city'];
+											$purp[] = $row['purpose'];
+											$date[] = $row['date'];
+											$_SESSION['count1'] = mysqli_num_rows($res1);
+										}
+									}
+							}
+						
+				   ?>
+				<h4></h4>
+
+				<table  class='table table-stripped table-bordered' id = 'example1'>
+				<tr> 
+							
+ 							<td><strong>Faculty : <?php echo $_SESSION['fac']; ?></strong></td>
+							<td><strong>From :<?php echo $_SESSION['from_date']; ?></strong></td>
+							<td><strong>To :<?php echo $_SESSION['to_date']; ?></strong></td>
+							<td><strong>Visits Attended :<?php echo $_SESSION['count1']; ?></strong></td>
+							
+				</tr>	
+				</table>
+				<div style="margin:20px;">
+				<?php
+					for($i = 0; $i<$_SESSION['count1']; $i++)
+							{
+								echo "<table  class='table table-stripped table-bordered' id = 'example1'><td>";
+								echo "<strong>Faculty name :</strong>".$fname[$i]."<br>";
+								echo "<strong>Industrial Visit Details :</strong>".$ind[$i]."<br>";
+								echo "<strong>City :</strong>".$city[$i]."<br>";
+								echo "<strong>Purpose of Visit :</strong>" .$purp[$i]."<br>";
+								echo "<strong>Date :</strong>".$date[$i]."<br>";
+								echo "</td><table>";
+								echo "<br>";
+							}
+				?>
+				</div>
+				<?php
+						}
+				?>
+				</form>
+              </div>
