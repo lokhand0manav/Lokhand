b99ad186f438cc8bc7e4c4cfd72a5df0568abb31
lokhand0manav <nsankpal97@gmail.com>
2018-01-17 02:14:45 +0530

    integrated v2

---
+++ extc/IV/IVSql.php
@@ -0,0 +1,273 @@
+<?php
+//$_SESSION['f_id'] is created in index.php
+//here the connection is established so any changes in db only have to be reflected in connection function;
+	function connection()
+	{
+		$conn=mysqli_connect('localhost','root','','department');
+		return $conn;
+	}
+
+//this is simpleQuery, carrying simple structure of a query, call through various functions
+	function simpleQuery($what,$from,$where) 
+	{
+		$conn=connection();
+		$query = "SELECT $what from $from where $where";
+		$result=mysqli_query($conn,$query);
+		mysqli_close($conn);
+		return $result;
+	}
+
+//this is simple query returning only sql query, which is needed for export to excel
+	function simpleQueryReturn($what,$from,$where) 
+	{
+		$conn=connection();
+		$query = "SELECT $what from $from where $where";
+		mysqli_close($conn);
+		return $query;
+	}
+
+//get the faculty details from the facultydetails table 
+	function getFacultyDetails($f_id)
+	{
+		if(strcmp($f_id,"")==0)
+		{
+			return simpleQuery("*","facultydetails","1=1");	
+		}
+		else
+		{
+			return simpleQuery("*","facultydetails","Fac_ID = $f_id");
+		}
+	}
+
+//for returning count in view_menu
+	function count_query($from,$f_id,$isDate) 
+	{
+		if($_SESSION['username'] == 'hodextc@somaiya.edu' || $_SESSION['username'] == 'member@somaiya.edu')
+		{
+				if($isDate==0) //is the date mentioned? 0 means not
+				{	
+					if($f_id==0 || $f_id==9) //ALL, //9 for HOD
+						return simpleQuery("count('id') as total", $from ,"1=1");
+					else
+						return simpleQuery("count('id') as total", $from ,"f_id='".$f_id."'");
+				}
+				else
+				{
+					if($f_id==0 || $f_id==9) //ALL
+						return simpleQuery("count('id') as total", $from , "date >='".$GLOBALS['min_date']."' AND date <='".$GLOBALS['max_date']."'");		
+					else
+						return simpleQuery("count('id') as total", $from , "f_id='".$f_id."'AND date >='".$GLOBALS['min_date']."' AND date <='".$GLOBALS['max_date']."'");		
+
+				}
+	
+		}
+		else
+		{
+			if($isDate==0)
+			{
+				return simpleQuery("count('id') as total", $from ,"f_id = '".$f_id."'");
+
+			}
+			else
+			{
+			
+				return simpleQuery("count('id') as total", $from , "f_id='".$f_id."'AND date >='".$GLOBALS['min_date']."' AND date <='".$GLOBALS['max_date']."'"); //no where condition
+
+			}	
+		}
+		
+     
+	}
+
+//the name suggests, it is for view.php
+	function view($from,$f_id,$isDate)
+	{
+
+		if($_SESSION['username'] == 'hodextc@somaiya.edu' || $_SESSION['username'] == 'member@somaiya.edu')
+		{
+			
+				if($isDate==0)
+				{
+					if($f_id==0) //ALL
+						return simpleQuery("ind,city,purpose,date",$from,"1=1 ORDER BY DATE ASC");
+					else
+						return simpleQuery("ind,city,purpose,date",$from,"f_id = '".$f_id."' ORDER BY DATE ASC");
+				}
+				else
+				{
+					if($f_id==0) //ALL faculty
+						return simpleQuery("ind,city,purpose,date",$from,"date >='".$GLOBALS['min_date']."' AND date <='".$GLOBALS['max_date']."' ORDER BY DATE ASC");
+					else
+						return simpleQuery("ind,city,purpose,date",$from,"f_id = '".$f_id."' AND date >='".$GLOBALS['min_date']."' AND date <='".$GLOBALS['max_date']."' ORDER BY DATE ASC");
+
+				}
+		
+		}
+		else
+		{
+			if($isDate==0)
+			{
+				return simpleQuery("ind,city,purpose,date",$from,"f_id = '".$f_id."' ORDER BY DATE ASC");
+			}
+			else
+			{
+				
+				return simpleQuery("ind,city,purpose,date",$from,"f_id='".$f_id."'AND date >='".$GLOBALS['min_date']."' AND date <='".$GLOBALS['max_date']."' ORDER BY DATE ASC"); //no where condition
+			}
+
+		}	
+	}
+
+
+//for returning the sql query in view, which is required in export to excel
+	function viewReturn($from,$f_id,$isDate) 
+	{
+
+		if($_SESSION['username'] == 'hodextc@somaiya.edu' || $_SESSION['username'] == 'member@somaiya.edu')
+		{
+			
+				if($isDate==0)
+				{
+					if($f_id==0) //ALL faculty
+						return simpleQueryReturn("ind,city,purpose,date",$from,"1=1 ORDER BY DATE ASC");
+					else
+						return simpleQueryReturn("ind,city,purpose,date",$from,"f_id = '".$f_id."' ORDER BY DATE ASC");	
+				}
+				else
+				{
+					if($f_id==0) //ALL faculty
+						return simpleQueryReturn("ind,city,purpose,date",$from,"date >='".$GLOBALS['min_date']."' AND date <='".$GLOBALS['max_date']."' ORDER BY DATE ASC");	
+					else
+						return simpleQueryReturn("ind,city,purpose,date",$from,"f_id = '".$f_id."' AND date >='".$GLOBALS['min_date']."' AND date <='".$GLOBALS['max_date']."' ORDER BY DATE ASC");
+
+				}
+			
+		}
+		else
+		{
+			if($isDate==0)
+			{
+
+				return simpleQueryReturn("ind,city,purpose,date",$from,"f_id = '".$f_id."' ORDER BY DATE ASC");
+			}
+			else
+			{
+		
+				return simpleQueryReturn("ind,city,purpose,date",$from,"f_id='".$f_id."'AND date >='".$GLOBALS['min_date']."' AND date <='".$GLOBALS['max_date']."' ORDER BY DATE ASC"); //no where condition
+			}
+
+		}	
+	}
+
+//both for attended and organized and admin aswell, in edit.php
+	function edit($from,$f_id) 
+	{
+
+		if($_SESSION['username'] == 'hodextc@somaiya.edu' || $_SESSION['username'] == 'member@somaiya.edu')
+		{
+			if($f_id==0) //0 is for all
+				return simpleQuery("*",$from,"1=1");
+			else
+				return simpleQuery("*",$from,"f_id = '".$f_id."'");
+		}
+		else
+		{
+			return simpleQuery("*",$from,"f_id = '".$f_id."'");
+		}
+	}
+
+//this is the core function, used in form.php,update.php,delete.php can be used in attended, organized and admin of both
+//*note for developer : for time being all the if else statement(HOD) have same blocks of statement, if no changes are found remove*	
+	function IV($what,$from,$arr,$select)
+	{
+
+		switch($select)
+		{
+			case "select" :  
+							 	if(strcmp($from,"IV_attended")==0)
+							 		return simpleQuery($what,$from,"id= $arr");
+							 	else
+							 		return simpleQuery($what,$from,"id= $arr");
+							 		
+							 
+							 break;
+			
+			case "update" :  
+								if(strcmp($from,"IV_attended")==0)
+							 	{
+							 	
+										$sql = "UPDATE $from set f_id ='$arr[1]' ,ind ='$arr[2]', city='$arr[3]', purpose='$arr[4]', date='$arr[5]' where id= $arr[0];";
+							 			$result=mysqli_query($GLOBALS['conn'],$sql);
+							 			mysqli_close($GLOBALS['conn']);
+										return $result;		
+							 			
+							 	}
+							    else //organized
+							 	{
+							 		
+										$sql = "UPDATE $from set f_id ='$arr[1]', ind ='$arr[2]', city='$arr[3]', purpose='$arr[4]', date='$arr[5]',t_audience='$arr[6]',staff='$arr[7]',t_from='$arr[12]',t_to='$arr[13]' where id= $arr[0];";
+							 			$result=mysqli_query($GLOBALS['conn'],$sql);
+							 			mysqli_close($GLOBALS['conn']);
+										return $result;			
+							 	}
+							 break;
+			
+			case "insert" :  
+								if(strcmp($from,"IV_attended")==0)
+								{
+								
+										//$conn=connection();
+										$sql="INSERT INTO $from (f_id,ind,city,purpose,date) VALUES ('$arr[1]','$arr[2]','$arr[3]','$arr[4]','$arr[5]')";
+										$result=mysqli_query($GLOBALS['conn'],$sql);
+										return $result;	
+									
+								}	
+								else
+								{
+					
+										$sql="INSERT INTO $from (f_id,ind,city,purpose,date,t_audience,staff,t_from,t_to) VALUES ('$arr[1]','$arr[2]','$arr[3]','$arr[4]','$arr[5]','$arr[6]','$arr[7]','$arr[12]','$arr[13]')";
+										$result=mysqli_query($GLOBALS['conn'],$sql);
+										return $result;	
+								
+								}	
+								break;
+
+			case "delete" : 	if(strcmp($from,"IV_attended")==0)
+								{
+										$sql="DELETE FROM $from WHERE id='$arr'";
+										$result=mysqli_query($GLOBALS['conn'],$sql);
+							 			mysqli_close($GLOBALS['conn']);
+										return $result;	
+									
+									
+								}
+							 else
+							 	{
+							 		
+										$sql="DELETE FROM $from WHERE id='$arr'";
+										$result=mysqli_query($GLOBALS['conn'],$sql);
+							 			mysqli_close($GLOBALS['conn']);
+										return $result;	
+										
+							 	}
+							 break;			 
+		}
+
+	}
+
+
+
+	//create a query function with one of the argument describing:
+		//1. whether it is view.php or not, 
+			//for view.php we only have few comuns to be shown and there is order by clause used
+	    //2.count query, this will count the query, an argument specifying : count using date or not
+	//it seems like count_query and view_menu are same
+	//edit form and updated will have an argument specifying attended or organized
+	   //edit will only have : simple query with argument specifying faculty id *in og we used faculty name*
+	   //form, in this we haveto select that particular row which we are supposed to edit, sent from edit.php
+		//so we will need a row id (attended_id or organized_id or just simply row_id)
+		//apart from this, we have to update and insert values using form.php file, so
+			//an update query with arguments for either attended or organized(use switch case) and row_id 
+ 			//an Insert query will insert the values, argument : either attended or organzied(switch case) and faculty id 
+	//update.php 	
+ ?>	
---
+++ extc/IV/upload.php
@@ -0,0 +1,160 @@
+<?php
+if(session_status() == PHP_SESSION_NONE)
+	{
+    //session has not started
+     session_start();
+    }
+//check if user has logged in or not
+ if(!isset($_SESSION['username'])){
+    //send the iser to login page
+    header("location:index.php");
+    }
+
+//setting error variables
+$error="";
+$conn=connection();
+//for which id=id, type=attended or organized and for the fine="permission/report or other"
+  
+  $type = $_SESSION['type'] ; 
+  $id   = $_SESSION['id']   ; 
+  $file = $_SESSION['file'] ; 
+
+
+//check if the insert was pressed
+//QUERY for the applicable not applicabe will be found here
+if(isset($_POST['insert-image']))
+  {
+	$success =0;
+	//$_SESSION['applicable'] = $_POST['applicable'];
+	
+	if(isset($_POST['applicable']))
+	{
+		if($_POST['applicable'] == 2)
+		{
+            	$query = "UPDATE $type SET $file='NULL' where id='$id'"; 
+             
+            mysqli_query($conn,$query);
+			$success =1;
+			 
+		}
+		else if($_POST['applicable'] == 3)
+		{
+            	$query = "UPDATE $type SET $file='not_applicable' where id='$id'";
+            mysqli_query($conn,$query);
+			 			 $success =1;
+
+			
+		}
+		else if($_POST['applicable'] == 1)
+		{
+			if(isset($_FILES['image']))
+			{
+			  $errors= array();
+			  $fileName = $_FILES['image']['name'];
+			  $fileSize = $_FILES['image']['size'];
+			  $fileTmp = $_FILES['image']['tmp_name'];
+			  $fileType = $_FILES['image']['type'];
+			  //$fileExt=strtolower(end(explode('.',$_FILES['image']['name'])));
+			  $targetName="IV/uploads/".$type."/".$file."/".$fileName;  
+			  $targetPath="IV/uploads/".$type."/".$file."/";
+			  
+			  if(empty($errors)==true) {
+				if (file_exists($targetName)) {   
+					unlink($targetName);
+				}      
+				 $moved = move_uploaded_file($fileTmp,$targetName);
+				 if($moved == true)
+				 {
+					 //successful
+						$query = "UPDATE $type SET $file='".$targetName."' where id='$id'"; 
+					 mysqli_query($conn,$query);
+					 			 $success =1;
+
+					
+					 //echo "<h1> $targetName </h1>";
+				 }
+				 else{
+					 //not successful
+					 //header("location:error.php");
+					 
+				 }
+			  }
+				else{
+				 print_r($errors);
+				//header("location:else.php");
+			  }
+			}
+		}
+	
+}
+	unset($_SESSION['type']);
+	unset($_SESSION['file']);
+	unset($_SESSION['id']);
+	
+	if(strcmp($type,"IV_attended")==0)			
+	 header("location:template.php?x=IV/select_menu/edit_menu.php&alert=update&type=attended");	
+	else 
+	 header("location:template.php?x=IV/select_menu/edit_menu.php&alert=update&type=organized");	
+}
+	
+
+
+if(isset($_POST['cancel'])){
+	header("location:template.php?x=IV/select_menu/edit_menu.php");
+}
+?>
+
+
+
+
+
+
+<?php 
+include('scripting.php');
+ ?>
+
+
+
+
+ 			<div class="col-md-6">
+              <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Upload Paper</h3>
+                </div><!-- /.box-header -->
+                <!-- form start -->
+                <form action="" method="POST" enctype="multipart/form-data" class="row">
+
+                    <input type ='hidden' name = 'id' value = '<?php echo $cardId;?>'>
+                      <div class="form-group col-md-6">
+
+						<label for="course">Applicable ?<br></label>
+						<br>	
+							<input type='radio' name='applicable' class='non-vac' value='1' > Yes <br>
+							<input type='radio' name='applicable' class='vac' value='2' > Applicable, but not yet available <br>
+						 	<input type='radio' name='applicable' class='vac' value='3' > No <br>
+					  </div>
+					  <div class='second-reveal'>
+					    <div class="form-group col-md-6">
+					 	
+                         <label for="card-image">Paper * </label>
+                         <input  type="file"   class="form-control input-lg" id="card-image" name="image">
+                    	</div> 
+					  </div>
+                    
+                    <div class="form-group col-md-12">
+                <!--       <button name="cancel" type="submit" class="btn btn-warning btn-lg">Cancel</button> -->
+		 
+                         <div class="pull-right"> 
+						 
+                             <button name="insert-image" type="submit" class="btn btn-success  btn-lg">Insert</button>
+                         </div>
+                    </div> 
+                 </form>
+                
+                </div>
+            </div>
+  
+   
+
+   
+   
